774c93bf0b6bab40b227562e60729a9f86d9205f
chaloxc <gonzalo.baldassarre@gmail.com>
2020-09-06 19:25:06 -0300

    Agrego pantalla con requerimientos

--- src/main/java/com/batcheador/Batcheador.java
+++ src/main/java/com/batcheador/Batcheador.java
@@ -1,48 +1,86 @@
 package com.batcheador;
-import java.awt.*; 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
+
 import javax.swing.*;
 
 public class Batcheador {
     public static void createWindow() {
-        /*
-         * Creo ventana(JFrame)
-         */
     	JFrame frame = new JFrame("Batcheador");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
         
-        /*
-         * Creo labels y sus correspondientes componentes, esto
-         * deberia hacerse en base a los parametro que deberia recibir el
-         * batcheador. 
-         */
         JLabel comboBoxLabel = new JLabel("Combo box", SwingConstants.LEFT);
-        comboBoxLabel.setPreferredSize(new Dimension(100,50));
-        // Acá habria que agregar un JComboBox
-        
+        comboBoxLabel.setPreferredSize(new Dimension(100,30));
+        JComboBox comboBox = new JComboBox();
+        comboBox.addItem("Opcion 1");
+        comboBox.addItem("Opcion 2");
+        comboBox.addItem("Opcion 3");
+        comboBox.setPreferredSize(new Dimension(100,30));
+
         JLabel textFieldLabel = new JLabel("Text field", SwingConstants.LEFT);
-        textFieldLabel.setPreferredSize(new Dimension(100,50));
-        // Acá habria que agregar un JTextField
+        textFieldLabel.setPreferredSize(new Dimension(100,30));
+        JTextField textField = new JTextField();
+        textField.setEditable(false);
+        
+        comboBox.addItemListener(new ItemListener() {
+            /*
+             * Funcion para sobreescribir el textField ante el evento
+             * de cambio de seleccion en el comboBox(listener)
+             */
+        	@Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED) {
+                    textField.setText((String) comboBox.getSelectedItem());
+                }
+            }
+        });
+        textField.setPreferredSize(new Dimension(100,30));
 		
+        final JFileChooser fc = new JFileChooser();
         JLabel fileChooserLabel = new JLabel("File chooser", SwingConstants.LEFT);
-        fileChooserLabel.setPreferredSize(new Dimension(100,50));
-        // Acá habria que agregar un JButton que genere un JFileChooser 
+        fileChooserLabel.setPreferredSize(new Dimension(100,30));
+        JButton fileChooserButton = new JButton("Examinar");
         
-        /*
-         * Agrego todo lo que cree al frame(ventana)
-         */
-        frame.getContentPane().add(comboBoxLabel, BorderLayout.PAGE_START);
-        frame.getContentPane().add(textFieldLabel, BorderLayout.CENTER);
-        frame.getContentPane().add(fileChooserLabel, BorderLayout.PAGE_END);
+        fileChooserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	int returnVal = fc.showOpenDialog(frame);
+            	 
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                	File file = fc.getSelectedFile();
+                	System.out.println(file.getName());
+                } else {
+                	System.out.println("No aprobado ?");
+                }
+            }
+        });
         
+        fileChooserButton.setPreferredSize(new Dimension(100,30));
+   
+        panel.add(comboBoxLabel, BorderLayout.PAGE_START);
+        panel.add(comboBox, BorderLayout.PAGE_END);
         
-        /*
-         * seteo , hago un pack(?) creo que para que se valide todo
-         * lo que agregué o no sé, no lei bien la docu pero se que hay que hacerlo
-         * y lo muestro
-         */
+        JPanel panel2 = new JPanel();
+        panel2.add(textFieldLabel, BorderLayout.PAGE_START);
+        panel2.add(textField, BorderLayout.PAGE_END);
+        
+        JPanel panel3 = new JPanel();
+        panel3.add(fileChooserLabel, BorderLayout.PAGE_START);
+        panel3.add(fileChooserButton, BorderLayout.PAGE_END);
+        
+        Container ventana = frame.getContentPane();
+        ventana.add(panel, BorderLayout.PAGE_START);
+        ventana.add(panel2, BorderLayout.CENTER);
+        ventana.add(panel3, BorderLayout.PAGE_END);
+        
         frame.setLocationRelativeTo(null);
         frame.pack();
-        frame.setSize(100,150);
+        frame.setSize(300,150);
         frame.setVisible(true);
     
     }
