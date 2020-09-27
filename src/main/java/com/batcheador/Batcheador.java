944a588a175d7212a9992ce7b6a1e9248cccdfcc
chaloxc <gonzalo.baldassarre@gmail.com>
2020-09-27 20:19:31 -0300

    Agrego nueva implementacion de batcheador con annotations

--- src/main/java/com/batcheador/Batcheador.java
+++ src/main/java/com/batcheador/Batcheador.java
@@ -1,89 +1,70 @@
 package com.batcheador;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.io.File;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import com.annotations.*;
 
 public class Batcheador {
-    public static void createWindow() {
-    	JFrame frame = new JFrame("Batcheador");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
-        
-        JLabel comboBoxLabel = new JLabel("Combo box", SwingConstants.LEFT);
-        comboBoxLabel.setPreferredSize(new Dimension(100,30));
-        JComboBox comboBox = new JComboBox();
-        comboBox.addItem("Opcion 1");
-        comboBox.addItem("Opcion 2");
-        comboBox.addItem("Opcion 3");
-        comboBox.setPreferredSize(new Dimension(100,30));
-
-        JLabel textFieldLabel = new JLabel("Text field", SwingConstants.LEFT);
-        textFieldLabel.setPreferredSize(new Dimension(100,30));
-        JTextField textField = new JTextField();
-        textField.setEditable(false);
-        
-        comboBox.addItemListener(new ItemListener() {
-            /*
-             * Funcion para sobreescribir el textField ante el evento
-             * de cambio de seleccion en el comboBox(listener)
-             */
-        	@Override
-            public void itemStateChanged(ItemEvent e) {
-                if(e.getStateChange() == ItemEvent.SELECTED) {
-                    String selectedOption = (String) comboBox.getSelectedItem();
-                    textField.setText(selectedOption);
-                    System.out.println(selectedOption);
-                }
-            }
-        });
-        textField.setPreferredSize(new Dimension(100,30));
+	private List<Class> apps;
+	
+	public Batcheador(List<Class> applications) {
+			this.apps = applications;
+	}
+	
+    public void createWindow() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		JFrame frame = new JFrame("Batcheador");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-        final JFileChooser fc = new JFileChooser();
-        JLabel fileChooserLabel = new JLabel("File chooser", SwingConstants.LEFT);
-        fileChooserLabel.setPreferredSize(new Dimension(100,30));
-        JButton fileChooserButton = new JButton("Examinar");
-        
-        fileChooserButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	int returnVal = fc.showOpenDialog(frame);
-            	 
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                	File file = fc.getSelectedFile();
-                	System.out.println(file.getName());
-                } else {
-                	System.out.println("El usuario canceló");
-                }
-            }
-        });
-        
-        fileChooserButton.setPreferredSize(new Dimension(100,30));
-   
-        panel.add(comboBoxLabel, BorderLayout.PAGE_START);
-        panel.add(comboBox, BorderLayout.PAGE_END);
-        
-        JPanel panel2 = new JPanel();
-        panel2.add(textFieldLabel, BorderLayout.PAGE_START);
-        panel2.add(textField, BorderLayout.PAGE_END);
-        
-        JPanel panel3 = new JPanel();
-        panel3.add(fileChooserLabel, BorderLayout.PAGE_START);
-        panel3.add(fileChooserButton, BorderLayout.PAGE_END);
-        
-        Container ventana = frame.getContentPane();
-        ventana.add(panel, BorderLayout.PAGE_START);
-        ventana.add(panel2, BorderLayout.CENTER);
-        ventana.add(panel3, BorderLayout.PAGE_END);
-        
-        frame.setLocationRelativeTo(null);
-        frame.pack();
-        frame.setSize(300,150);
-        frame.setVisible(true);
+		for(Integer i=0; i<apps.size() ; i++) {
+			Class currentApplication = apps.get(i).getClass();
+			Annotation[] anotations = currentApplication.getDeclaredAnnotations();
+			// procesamos anotations
+			// TO DO
+			//
+			
+		    Field[] fields = currentApplication.getDeclaredFields();
+		    processFieldsAnnotations(fields, frame);
+			break;
+		}	
+		
+		frame.setLocationRelativeTo(null);
+		frame.pack();
+		frame.setSize(300,150);
+		frame.setVisible(true);
+    }
     
+    public void processFieldsAnnotations(Field[] fields, JFrame frame) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		for (int i=0; i<fields.length ; i++) {
+    		Annotation annotation = fields[i].getAnnotation(Parameter.class);
+    		// aca rompe
+    		String parameterType = (String) annotation.getClass().getField("type").get(annotation);
+    		
+    		switch(parameterType) {
+    			case "text":
+    				JLabel textFieldLabel = new JLabel("Text field", SwingConstants.LEFT);
+    		    	textFieldLabel.setPreferredSize(new Dimension(100,30));
+    		    	JTextField textField = new JTextField();
+    		       
+    		    	JPanel panel = new JPanel();
+    		    	panel.add(textFieldLabel, BorderLayout.PAGE_START);
+    		    	panel.add(textField, BorderLayout.PAGE_END);
+    		       
+    		    	Container ventana = frame.getContentPane();
+    		    	ventana.add(panel, BorderLayout.PAGE_START);
+	    		default: 
+	    			break;
+    		}
+    	}
+    	
     }
 }
--- src/main/java/com/batcheador/Main.java
+++ src/main/java/com/batcheador/Main.java
@@ -1,7 +1,16 @@
 package com.batcheador;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.applications.MuteVideo;
+
 public class Main {
-    public static void main(String[] args) {
-    	Batcheador.createWindow();
+    public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+    	List<Class> applications = new ArrayList<Class>();
+    	applications.add(MuteVideo.class);
+    	
+    	Batcheador batcheador = new Batcheador(applications);
+    	batcheador.createWindow();
     }
 }
